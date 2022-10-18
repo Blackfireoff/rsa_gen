dbc30dc8a86b7eb71b7efd2e487e89e5d19d0e6e
leonardoheron <leonardo.heron@etu.univ-st-etienne.fr>
2022-10-18 19:11:05 +0200

    change extension
    change extension

--- keys.py
+++ keys.py
@@ -1,10 +1,10 @@
 import rsa
 (pubkey,privkey) = rsa.newkeys(4096)
 
-fichier = open("cle_pub.LML","a")
+fichier = open("cle_pub.pem","a")
 fichier.write(str(pubkey))
 fichier.close()
 
-fichier = open("cle_priv.LML","a")
+fichier = open("cle_priv.pem","a")
 fichier.write(str(privkey))
 fichier.close()
--- keys_crypto.py
+++ keys_crypto.py
@@ -1,11 +1,11 @@
-from Crypto.PublicKey import rsa
+from Crypto.PublicKey import RSA
 
 key = RSA.generate(1024)
 
 with open('private.pem','w') as kf:
-	kf.write(k.decode())
+	kf.write(kf.decode())
 	kf.close()
 
 with open('public.pem','w') as pf:
-	pf.write(p.decode())
+	pf.write(pf.decode())
 	pf.close()
