e5319114e4acebf7c18019c0ae429a2f4fc46e4e
leonardoheron <leonardo.heron@etu.univ-st-etienne.fr>
2022-10-18 21:08:36 +0200

    final_push

--- crypt.py
+++ crypt.py
@@ -7,4 +7,12 @@ message = input("Type your message here : ").encode('utf-8')
 
 crypto = rsa.encrypt(message, publickey)
 
+#signature = rsa.sign(crypto,publickey, 'SHA-1')
+
+#if(rsa.verify(message, signature, publickey) ==  'SHA-1'):
+#    print("Signature vérifié")
+
+with open("message_crypt.LML", mode="wb") as f:
+    f.write(crypto)
+
 print(crypto)
--- decrypt.py
+++ decrypt.py
@@ -5,11 +5,9 @@ with open("cle_priv.pem", mode="rb") as cle:
     clepriv = cle.read()
 privatekey = rsa.PrivateKey.load_pkcs1(clepriv)
 
-with open("message_crypt.LML", mode="r") as f:
-    msg_crypt = f.read()
+msg_crypt = open("message_crypt.LML","rb").read()
 
-msg_crypt = bytes(msg_crypt, 'utf-8')
 
 msg_decrypt = rsa.decrypt(msg_crypt, privatekey)
-print(msg_decrypt)
+print(msg_decrypt.decode('utf-8'))
 print("decryptage \n")
